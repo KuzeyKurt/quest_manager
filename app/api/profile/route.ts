8b31603d4abd7de460d4f7cc5fd4178fb7bb61c2
Seyran Japarov <japarov.s.i20@gmail.com>
2026-05-05 04:12:27 +0300

    коммит обновлений и некоторых доработок

--- app/api/profile/route.ts
+++ app/api/profile/route.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from "@/lib/prisma-client";
 import { NextResponse } from "next/server";
 import { getSessionUserId } from "@/lib/session";
 
--- lib/profile.ts
+++ lib/profile.ts
@@ -1,4 +1,4 @@
-import { PrismaClient} from "@prisma/client"
+import { PrismaClient } from "./prisma-client"
 import { getSessionUserId } from "./session"
 
 const prisma = new PrismaClient();
